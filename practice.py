8ca2794ecf707bc65882f44fe208cc569cdc328a
Jeseong Lee <wptjd6141@naver.com>
2020-09-10 12:33:38 -0700

    파이썬 파일 입출력

--- practice.py
+++ practice.py
@@ -1,19 +1,4 @@
-print("{0: >8}".format(1234))
-
-print("{0: >+8}".format(1234))
-print("{0: >+8}".format(-1234))
-
-print("{0:_<8}".format(1234))
-
-print("{0:,}".format(1234567890))
-
-print("{0:+,}".format(1234567890))
-print("{0:+,}".format(-1234567890))
-
-print("{0:_<+20,}".format(1234567890))
-
-print("{0:f}".format(10/3))
-
-print("{0:.3f}".format(10/3))
-
-print("{0:_<+20,.3f}".format(1234567890.1234))
+test_file = open("test.txt", "w", encoding="utf8")
+print("Hello,", file=test_file)
+print("Python!", file=test_file)
+test_file.close()
