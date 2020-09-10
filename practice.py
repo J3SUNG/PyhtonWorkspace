5353033e0f1636ce4cf8f0cadb4d9ed2fb3c6aeb
Jeseong Lee <wptjd6141@naver.com>
2020-09-10 12:30:08 -0700

    파이썬 공부

--- practice.py
+++ practice.py
@@ -1,7 +1,19 @@
-name = "OWEN"
-def funcPrint():
-    global name
-    name = "mr." + name
-    print(name)
+print("{0: >8}".format(1234))
 
-funcPrint()
+print("{0: >+8}".format(1234))
+print("{0: >+8}".format(-1234))
+
+print("{0:_<8}".format(1234))
+
+print("{0:,}".format(1234567890))
+
+print("{0:+,}".format(1234567890))
+print("{0:+,}".format(-1234567890))
+
+print("{0:_<+20,}".format(1234567890))
+
+print("{0:f}".format(10/3))
+
+print("{0:.3f}".format(10/3))
+
+print("{0:_<+20,.3f}".format(1234567890.1234))
