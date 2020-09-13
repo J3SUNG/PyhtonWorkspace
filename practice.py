ef88602be03a272b83a2d2f9d63acb6e6a9c39ef
Jeseong Lee <wptjd6141@naver.com>
2020-09-12 22:36:58 -0700

    pickle

--- practice.py
+++ practice.py
@@ -8,3 +8,22 @@ test_file.write("Nice to ")
 test_file.write("Meet You")
 print("Python!", file=test_file)
 test_file.close()
+
+test_file = open("test.txt", "r", encoding="utf8")
+print(test_file.read())
+test_file.close()
+
+test_file = open("test.txt", "r", encoding="utf8")
+while True:
+    line = test_file.readline()
+    if not line:
+        break
+    print(line, end="")
+test_file.close()
+print()
+
+test_file = open("test.txt", "r", encoding="utf8")
+lines = test_file.readlines()
+for line in lines:
+    print(line, end="")
+test_file.close()
