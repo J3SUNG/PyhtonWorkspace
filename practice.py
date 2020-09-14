d4e510ef7de592a053475030f2d52805eed4109d
Jeseong Lee <wptjd6141@naver.com>
2020-09-14 12:14:40 -0700

    파이썬

--- practice.py
+++ practice.py
@@ -1,25 +1,34 @@
 class People:
-    def __init__(self, name, age, weight):
+    def __init__(self, name, age, weight, run_speed):
         self.name = name
         self.age = age
         self.weight = weight
+        self.run_speed = run_speed
         print("{0}이 입장하였습니다.".format(self.name))
         print("나이는 {0}, 몸무게는 {1} 입니다.".format(self.age, self.weight))
+    
+    def move(self):
+        print("{0}이 {1}속도로 달리고있습니다.".format(self.name, self.run_speed))
 
 class Car:
     def __init__(self, speed):
         self.speed = speed
 
-    def drive(self, name):
-        print("{0}이 {1}속도로 주행중입니다.".format(name, self.speed))
+    def drive(self):
+        print("{0}이 {1}속도로 주행중입니다.".format(self.name, self.speed))
 
 class detailPeople(People, Car):
-    def __init__(self, name, age, weight, height, speed):
-        People.__init__(self, name, age, weight)
-        Car.__init__(self, speed)
+    def __init__(self, name, age, weight, height):
+        People.__init__(self, name, age, weight, "0km/h")
+        Car.__init__(self, "0km/h")
         self.height = height
+        self.flight_speed = "200km/h"
+    
+    def fly(self):
+        print("{0}이 {1}속도로 비행 하고 있습니다.".format(self.name, self.flight_speed))
 
-guest_3 = detailPeople("June", 20, 70, 170, "100km/h")
-print("이름 : {0}, 나이 : {1}, 몸무게 : {2})".format(guest_3.name, guest_3.age, guest_3.weight))
-print("키 : {0}, 속도 : {1}".format(guest_3.height, guest_3.speed))
-guest_3.drive(guest_3.name)
+    def move(self):
+        self.fly()
+
+guest_3 = detailPeople("June", 20, 70, 170)
+guest_3.move()
