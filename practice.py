7912a3b7b46ba73cf48cce18d460b20b10a7b45b
Jeseong Lee <wptjd6141@naver.com>
2020-09-14 11:21:07 -0700

    파이썬

--- practice.py
+++ practice.py
@@ -1,29 +1,35 @@
-test_file = open("test.txt", "w", encoding="utf8")
-print("Hello,", file=test_file)
-print("Python!", file=test_file)
-test_file.close()
+class People:
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        print("{0}이 입장하였습니다.".format(self.name))
+        print("나이는 {0}, 몸무게는 {1} 입니다.".format(self.age, self.weight))
 
-test_file = open("test.txt", "a", encoding="utf8")
-test_file.write("Nice to ")
-test_file.write("Meet You")
-print("Python!", file=test_file)
-test_file.close()
+    def eat(self, food):
+        print("{0}이 {1}을 먹습니다.".format(self.name, food))
+        self.increase()
 
-test_file = open("test.txt", "r", encoding="utf8")
-print(test_file.read())
-test_file.close()
+    def increase(self):
+        print("몸무게가 {0}에서 {1}로 증가되었습니다.".format(self.weight, self.weight + 1))
+        self.weight += 1
 
-test_file = open("test.txt", "r", encoding="utf8")
-while True:
-    line = test_file.readline()
-    if not line:
-        break
-    print(line, end="")
-test_file.close()
-print()
+class detailPeople(People):
+    def __init__(self, name, age, weight, height):
+        People.__init__(self, name, age, weight)
+        self.height = height
 
-test_file = open("test.txt", "r", encoding="utf8")
-lines = test_file.readlines()
-for line in lines:
-    print(line, end="")
-test_file.close()
+guest_1 = People("OWEN", 23, 80)
+guest_2 = People("JOLLY", 19, 52)
+
+print("이름 : {0}, 나이 : {1}, 몸무게 : {2}".format(guest_1.name, guest_1.age, guest_1.weight))
+
+guest_1.adult = True
+if guest_1.adult == True:
+    print("{0}은 성인입니다.".format(guest_1.name))
+
+guest_1.eat("치킨")
+print(guest_1.weight)
+
+guest_3 = detailPeople("June", 20, 70, 170)
+print("이름 : {0}, 나이 : {1}, 몸무게 : {2}, 키 : {3}".format(guest_3.name, guest_3.age, guest_3.weight, guest_3.height))
